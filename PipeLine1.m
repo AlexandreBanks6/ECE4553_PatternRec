e8f4b88e1665523bbf7c8147f433c2dbfd58ebe2
AlexandreBanks6 <65726682+AlexandreBanks6@users.noreply.github.com>
2021-11-09 13:21:05 -0400

    Update
    Added Dataset and pipeline1 code

---
+++ PipeLine1.m
@@ -0,0 +1,13 @@
+%{
+Title: PipeLine1
+Author: Alexandre Banks, Bailey Christian Morrel
+Description:
+Date:November 9th, 2021
+Affiliation: University of New Brunswick, ECE 4553
+
+%}
+
+
+
+%% Data Processing
+
